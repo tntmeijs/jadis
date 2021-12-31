089ca12c98fa95ed761c2945b52ce2b0a86c20e0
tntmeijs <tntmeijs@gmail.com>
2021-12-31 15:39:43 +0100

    fix: generate documentation script should not override the cname file

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,4 +1,5 @@
 cargo doc --no-deps
 rm -rf ./docs
 echo "<meta http-equiv=\"refresh\" content=\"0; url=jadis\">" > target/doc/index.html
+echo "jadis.tahar.dev" > target/doc/CNAME
 cp -r target/doc ./docs
