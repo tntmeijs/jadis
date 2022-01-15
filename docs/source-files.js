14d0c70114cf4ce2f99f97298aba32c8a1af3a24
tntmeijs <tntmeijs@gmail.com>
2022-01-15 23:17:57 +0100

    docs: update documentation

--- docs/source-files.js
+++ docs/source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["classfile"] = {"name":"","files":["access_flags.rs","attribute.rs","byte_reader.rs","constant_pool.rs","field.rs","lib.rs","method.rs","utils.rs"]};
 sourcesIndex["disassembler"] = {"name":"","files":["disassembler.rs","main.rs"]};
-sourcesIndex["jadis"] = {"name":"","dirs":[{"name":"flags","files":["class_access_flags.rs","field_access_flags.rs","method_access_flags.rs","method_parameter_access_flags.rs","mod.rs","module_exports_flags.rs","module_flags.rs","module_opens_flags.rs","module_requires_flags.rs","nested_class_access_flags.rs"]}],"files":["attribute.rs","byte_reader.rs","class_file.rs","constant_pool.rs","disassembler.rs","field.rs","main.rs","method.rs","utils.rs"]};
+sourcesIndex["jadis"] = {"name":"","dirs":[{"name":"classfile","files":["attribute.rs","class_file.rs","constant_pool.rs","field.rs","method.rs","mod.rs"]},{"name":"flags","files":["class_access_flags.rs","field_access_flags.rs","method_access_flags.rs","method_parameter_access_flags.rs","mod.rs","module_exports_flags.rs","module_flags.rs","module_opens_flags.rs","module_requires_flags.rs","nested_class_access_flags.rs"]}],"files":["byte_reader.rs","disassembler.rs","main.rs","utils.rs"]};
 createSourceSidebar();
